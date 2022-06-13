fd461d70cdeacd6c1f789b1121ca8feab068f8c5
FireSkyGlobal <admin@firesky.cc>
2022-06-14 00:48:52 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <div className="header-section">
         <span className="title">
-          Live Class Room
+          Hacheo - Live Class Room
         </span>
       </div>
     </header>
